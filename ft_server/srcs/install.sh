b4b63ddce9d8264be4aaedb134a99dcee0f44e94
user42 <hk.dev22@gmail.com>
2021-02-14 12:45:09 +0100

    ft_server: autoindex

--- ft_server/srcs/install.sh
+++ ft_server/srcs/install.sh
@@ -3,6 +3,15 @@ mkdir /etc/nginx/ssl
 chmod 700 /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt -subj "/C=FR/ST=Paris/L=Franconville/O=42/OU=hkortbi/CN=localhost"
 
+
+# Config nginx and autoindex
+if [ "$AUTOINDEX" = "off" ];
+    then mv ./default_off etc/nginx/sites-enabled/
+    else mv ./default etc/nginx/sites-enabled/
+fi
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
 # Config mysql
 service mysql start
 echo "create database wordpress default character set utf8 collate utf8_unicode_ci;" | mysql -u root
